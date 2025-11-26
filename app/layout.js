bbf150da16a2a455e07e2c66ada0011febc30ca8
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-26 16:55:23 +0530

    AI Event Organizer

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 
@@ -9,11 +10,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white`}
       >
-        <main className="min-h-screen">{children}</main>
+        <ThemeProvider>
+        <main className="relative min-h-screen container mx-auto pt-40 md:pt-32">
+          {/* glow */}
+          <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                  <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+                  <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
+                </div>
+          <div className="relative z-10">{children}</div>
+          {/* footer */}
+        </main>
+        </ThemeProvider>
       </body>
     </html>
   );
