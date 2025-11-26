6f6ed98f747d448f678b8fa5a8e352ecd5d3c712
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-26 18:46:57 +0530

    AI Event Organizer

---
+++ components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <nav>
+        <div>
+            {/* Logo */}
+
+            {/* Search and location for logo */}
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Header
