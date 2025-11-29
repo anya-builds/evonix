68823db23c214eb4b0a7127950aeb96194b18761
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-11-29 18:50:40 +0530

    AI Event Organiser

--- components/header.jsx
+++ components/header.jsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import React from 'react'
 import Link from "next/link";
 import { SignInButton, useAuth,SignUpButton, UserButton,SignedOut,SignedIn, useUser } from "@clerk/nextjs";
+import { User } from 'lucide-react';
+import { Button } from './ui/button';
 const Header = () => {
   return (
     <>
@@ -25,16 +27,14 @@ const Header = () => {
           
           <div className="flex items-center">
               <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+                <SignInButton mode="modal">
+                  <Button size="sm">Sign In</Button>
+                </SignInButton>
+              </SignedOut>
+             <SignedIn>
+              <UserButton/>
+              </SignedIn>
+    
           </div>
             
         </div>
